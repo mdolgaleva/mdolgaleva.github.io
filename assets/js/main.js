a272edadd40708d26fb21bd2e4c9f4a970b340d3
mdolgaleva <clockworkchinotto@gmail.com>
2025-05-18 19:21:47 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,4 +96,4 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .skills__container, .contact__input',{interval: 200}); 
-sr.reveal('.accordion', { interval: 200 }); 
+sr.reveal('.accordion__content', { interval: 200 }); 
